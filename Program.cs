9d0f84671c35b6c3e4b63adcc31bd24040883db8
Paprota <paprota404@gmail.com>
2024-06-27 22:59:40 +0200

    Regex

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using Xceed.Words.NET;
+
+class Program{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Directory.GetCurrentDirectory());
+        string filePath = @"Animus\Animus_Jedno_życie\Animus_Jedno_życie_tekst.docx";
+
+        string pattern = @"Tytuł Polski LUB : (.*?) Tytuł Oryginalny \(jeśli jest\):";
+
+
+        using(DocX document  = DocX.Load(filePath))
+        {
+            string text = document.Text;
+            Console.WriteLine(text);   
+        }
+    }
+}
