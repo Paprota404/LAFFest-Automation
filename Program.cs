2dfbcc8c53c415f79fa1599d247fae317155c3fa
Paprota <paprota404@gmail.com>
2024-06-28 22:21:45 +0200

    API

--- Program.cs
+++ Program.cs
@@ -1,20 +1,60 @@
 ﻿using System;
 using System.IO;
 using Xceed.Words.NET;
+using System.Text.RegularExpressions;
 
-class Program{
+class Program
+{
     static void Main(string[] args)
     {
         Console.WriteLine(Directory.GetCurrentDirectory());
         string filePath = @"Animus\Animus_Jedno_życie\Animus_Jedno_życie_tekst.docx";
 
-        string pattern = @"Tytuł Polski LUB : (.*?) Tytuł Oryginalny \(jeśli jest\):";
+        string tytulPattern = @"TYTUŁ POLSKI LUB : (.*?)TYTUŁ ORYGINALNY";
+        string rezyseriaPattern = @"REŻYSER/KA: (.*?)PRODUKCJA";
+        string produkcjaPattern = @"ROK, CZAS TRWANIA: (.*?), (\d{4})";
+        string czasPattern = @", (\d{1,})’";
+        string opisPattern = @"OPIS FILMU:(.*?)(?=NAGRODY|$)";
+        string nagrodyPattern = @"NAGRODY I FESTIWALE:(.*)";
 
+        RegexOptions options = RegexOptions.Singleline | RegexOptions.IgnoreCase;
 
-        using(DocX document  = DocX.Load(filePath))
+        using (DocX document = DocX.Load(filePath))
         {
             string text = document.Text;
-            Console.WriteLine(text);   
+            Console.WriteLine(text);
+            Match tytulMatch = Regex.Match(text, tytulPattern, options);
+            Match rezyseriaMatch = Regex.Match(text, rezyseriaPattern, options);
+            Match produkcjaMatch = Regex.Match(text, produkcjaPattern, options);
+            Match czasMatch = Regex.Match(text, czasPattern, options);
+            Match opisMatch = Regex.Match(text, opisPattern, options);
+            Match nagrodyMatch = Regex.Match(text, nagrodyPattern, options);
+            string tytul = tytulMatch.Groups[1].Value.Trim();
+            Console.WriteLine(tytul);
+
+
+            string rezyseria = rezyseriaMatch.Groups[1].Value.Trim();
+            Console.WriteLine(rezyseria);
+
+            string produkcja = $"{produkcjaMatch.Groups[1].Value.Trim()}, {produkcjaMatch.Groups[2].Value.Trim()}";
+            Console.WriteLine(produkcja);
+
+            string czas = czasMatch.Groups[1].Value.Trim();
+            int totalMinutes = int.Parse(czas); // Convert the string to integer
+
+            int hours = totalMinutes / 60; // Get the number of hours
+            int minutes = totalMinutes % 60; // Get the remaining minutes
+
+            // Construct the output string
+            string formattedTime = $"{hours} H {minutes} Min";
+
+            Console.WriteLine(formattedTime);
+            
+            string opis = opisMatch.Groups[1].Value.Trim();
+            Console.WriteLine(opis);
+
+            string nagrody = nagrodyMatch.Groups[1].Value.Trim();
+            Console.WriteLine(nagrody);
         }
     }
 }
--- obj/Debug/net8.0/GoogleDriveToWordPress.AssemblyInfo.cs
+++ obj/Debug/net8.0/GoogleDriveToWordPress.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoogleDriveToWordPress")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a67f9cdad6fa3097fc094c07cda5d19de2a3987")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfdaf6933096298831f4f007d6c4ce1733428886")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoogleDriveToWordPress")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoogleDriveToWordPress")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
